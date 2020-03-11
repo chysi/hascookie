3e06f60bc19f85f3541497062eb529dc182d1c0d
2mol <2mol@users.noreply.github.com>
2020-03-11 11:28:12 +0100

    todo txt and formatting

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This code repo ideally:
 
 - is a very simple but functional example code base.
 - is somewhat well structured, to show how a "real world" Haskell project would look.
+- have a skeleton git repo for people to clone
 - takes shortcuts in order to allow for fast development.
     - sqlite instead of postgres
     - ignoring some type safety (initially) for SQL queries and form inputs
@@ -25,9 +26,16 @@ The API exposes the following features:
 
 ## Meetup abstract
 
-Title:
+Title: Workshop on building a webservice
 
-We will hold a workshop to build a webservice for a simple delivery service. This will include
+This is an interactive meetup, so bring your laptop! We will hold a workshop to build a webservice in Haskell. This includes writing a monolithic server that can serve webpages, process orders, save stuff in a database, and interact with external APIs.
+
+The meetup will be split into two parts:
+
+First we will show you some slides on the architecture and the libraries we intend to use. We will give you some pointers on how to set up your tooling, and we will tell you the features we think it makes sense to build. The default goal is to build a clone of https://cravecookie.com/, but you will be free to modify that to your own interests.
+
+Second you will pair up with somebody, and build out the application. Cazim, Juri, and maybe a few other Haskellers will be there to help you if you get stuck.
+
 
 ## references
 
--- src/Main.hs
+++ src/Main.hs
@@ -16,25 +16,25 @@ newtype MyAppState = DummyAppState (IORef Int)
 
 
 main :: IO ()
-main =
-    do ref <- newIORef 0
-       spockCfg <- SC.defaultSpockCfg EmptySession SC.PCNoDatabase (DummyAppState ref)
-       Spock.runSpock 8080 (Spock.spock spockCfg app)
+main = do
+    ref <- newIORef 0
+    spockCfg <- SC.defaultSpockCfg EmptySession SC.PCNoDatabase (DummyAppState ref)
+    Spock.runSpock 8080 (Spock.spock spockCfg app)
 
 app :: Spock.SpockM () MySession MyAppState ()
-app =
-    do Spock.get Spock.root $
-           Spock.html "<h1 style=\"color:red\">Hello World!</h1>"
+app = do
+    Spock.get Spock.root $
+        Spock.html "<h1 style=\"color:red\">Hello World!</h1>"
 
-       Spock.post "icanhascookie" $ do
-         params <- Spock.paramsPost
-         let maybeOrderData = parseOrderBody params
-         Spock.text $ maybe "Invalid form" (T.pack . show) maybeOrderData
+    Spock.post "icanhascookie" $ do
+        params <- Spock.paramsPost
+        let maybeOrderData = parseOrderBody params
+        Spock.text $ maybe "Invalid form" (T.pack . show) maybeOrderData
 
-       Spock.post "orderstatus" $ do
-         Spock.setStatus Http.status501
+    Spock.post "orderstatus" $ do
+        Spock.setStatus Http.status501
 
-       Spock.get ("hello" <//> Spock.var) $ \name ->
-           do (DummyAppState ref) <- Spock.getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-              Spock.text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+    Spock.get ("hello" <//> Spock.var) $ \name -> do
+        (DummyAppState ref) <- Spock.getState
+        visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
+        Spock.text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
