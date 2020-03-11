d8609045070bb723852b510526f24bc9f3cdd534
2mol <2mol@users.noreply.github.com>
2020-03-11 15:35:49 +0100

    more db stuff

--- src/DB.hs
+++ src/DB.hs
@@ -2,29 +2,44 @@
 
 module DB where
 
-import Control.Applicative ()
+import           Control.Applicative ()
 import qualified Data.Text as T
-import qualified Database.SQLite.Simple as SQL
-import           Database.SQLite.Simple (NamedParam(..))
-import           Database.SQLite.Simple.FromRow (FromRow)
+import qualified Data.Text.IO as TIO
+-- import           Database.SQLite.Simple (NamedParam(..))
+import qualified Database.SQLite.Simple as Sql
+-- import           Database.SQLite.Simple.FromRow (FromRow)
 
-data TestField = TestField Int T.Text deriving (Show)
+-- data TestField = TestField Int T.Text deriving (Show)
 
-instance FromRow TestField where
-    fromRow = TestField <$> SQL.field <*> SQL.field
+-- instance FromRow TestField where
+--     fromRow = TestField <$> Sql.field <*> Sql.field
 
-instance SQL.ToRow TestField where
-    toRow (TestField id_ str) = SQL.toRow (id_, str)
+-- instance Sql.ToRow TestField where
+--     toRow (TestField id_ str) = Sql.toRow (id_, str)
 
-main :: IO ()
-main = do
-    conn <- SQL.open "test.db"
-    SQL.execute_ conn "CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, str TEXT)"
-    SQL.execute conn "INSERT INTO test (str) VALUES (?)" (SQL.Only ("test string 2" :: String))
-    SQL.execute conn "INSERT INTO test (id, str) VALUES (?,?)" (TestField 13 "test string 3")
-    rowId <- SQL.lastInsertRowId conn
-    SQL.executeNamed conn "UPDATE test SET str = :str WHERE id = :id" [":str" := ("updated str" :: T.Text), ":id" := rowId]
-    r <- SQL.query_ conn "SELECT * from test" :: IO [TestField]
-    mapM_ print r
-    SQL.execute conn "DELETE FROM test WHERE id = ?" (SQL.Only rowId)
-    SQL.close conn
+
+
+-- main :: IO ()
+-- main = do
+--     conn <- Sql.open "test.db"
+--     Sql.execute_ conn "CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, str TEXT)"
+--     Sql.execute conn "INSERT INTO test (str) VALUES (?)" (Sql.Only ("test string 2" :: String))
+--     Sql.execute conn "INSERT INTO test (id, str) VALUES (?,?)" (TestField 13 "test string 3")
+--     rowId <- Sql.lastInsertRowId conn
+--     Sql.executeNamed conn "UPDATE test SET str = :str WHERE id = :id" [":str" := ("updated str" :: T.Text), ":id" := rowId]
+--     r <- Sql.query_ conn "SELECT * from test" :: IO [TestField]
+--     mapM_ print r
+--     Sql.execute conn "DELETE FROM test WHERE id = ?" (Sql.Only rowId)
+--     Sql.close conn
+
+init :: String -> IO Sql.Connection
+init dbFileName = do
+    conn <- Sql.open dbFileName
+    (ver : _ ) : _ <- Sql.query_ conn "select sqlite_version()" :: IO [[T.Text]]
+    TIO.putStrLn $ "sqlite version " <> ver
+    Sql.execute_ conn "create table if not exists orders (id integer primary key, amount integer)"
+    pure conn
+
+addOrder :: Sql.Connection -> Int -> IO ()
+addOrder conn amount = do
+    Sql.execute conn "insert into orders (amount) values (?)" (Sql.Only amount)
--- src/Main.hs
+++ src/Main.hs
@@ -2,19 +2,23 @@
 
 module Main where
 
--- import           Control.Monad.Trans (liftIO)
 -- import           Data.IORef (IORef, atomicModifyIORef', newIORef)
+import           Control.Monad.Trans (liftIO)
 import qualified Data.Text as T
+import           Database.SQLite.Simple (Connection)
+import qualified DB
 import qualified Network.HTTP.Types as Http
+import           Types
 import           Web.Spock ((<//>))
 import qualified Web.Spock as Spock
 import qualified Web.Spock.Config as SC
-import Types
 
 
 type ServerConn    = ()
 data ServerSession = ServerSession
 data ServerState   = ServerState
+    { dbConnection :: Connection
+    }
 
 type Server a = Spock.SpockM ServerConn ServerSession ServerState a
 
@@ -22,7 +26,10 @@ type Server a = Spock.SpockM ServerConn ServerSession ServerState a
 main :: IO ()
 main = do
     -- ref <- newIORef 0
-    spockCfg <- SC.defaultSpockCfg ServerSession SC.PCNoDatabase ServerState
+    dbConnection <- DB.init "orders.db"
+    -- DB.addOrder dbConnection 123
+    let serverState = ServerState { dbConnection = dbConnection }
+    spockCfg <- SC.defaultSpockCfg ServerSession SC.PCNoDatabase serverState
     Spock.runSpock 8080 (Spock.spock spockCfg app)
 
 
@@ -34,6 +41,11 @@ app = do
     Spock.post "icanhascookie" $ do
         params <- Spock.paramsPost
         let maybeOrderData = parseOrderBody params
+        if False
+            then do
+                (ServerState dbConnection) <- Spock.getState
+                liftIO $ DB.addOrder dbConnection 12345
+            else pure ()
         Spock.text $ maybe "Invalid form" (T.pack . show) maybeOrderData
 
     Spock.post "orderstatus" $ do
