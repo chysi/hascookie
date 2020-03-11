d0e394858de39df4d53eccd85348c67739f2ce05
2mol <2mol@users.noreply.github.com>
2020-03-11 11:46:40 +0100

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,8 @@
 
 module Main where
 
-import           Control.Monad.Trans (liftIO)
-import           Data.IORef (IORef, atomicModifyIORef', newIORef)
+-- import           Control.Monad.Trans (liftIO)
+-- import           Data.IORef (IORef, atomicModifyIORef', newIORef)
 import qualified Data.Text as T
 import qualified Network.HTTP.Types as Http
 import           Web.Spock ((<//>))
@@ -11,17 +11,22 @@ import qualified Web.Spock as Spock
 import qualified Web.Spock.Config as SC
 import Types
 
-data MySession = EmptySession
-newtype MyAppState = DummyAppState (IORef Int)
+
+type ServerConn    = ()
+data ServerSession = ServerSession
+data ServerState   = ServerState
+
+type Server a = Spock.SpockM ServerConn ServerSession ServerState a
 
 
 main :: IO ()
 main = do
-    ref <- newIORef 0
-    spockCfg <- SC.defaultSpockCfg EmptySession SC.PCNoDatabase (DummyAppState ref)
+    -- ref <- newIORef 0
+    spockCfg <- SC.defaultSpockCfg ServerSession SC.PCNoDatabase ServerState
     Spock.runSpock 8080 (Spock.spock spockCfg app)
 
-app :: Spock.SpockM () MySession MyAppState ()
+
+app :: Server ()
 app = do
     Spock.get Spock.root $
         Spock.html "<h1 style=\"color:red\">Hello World!</h1>"
@@ -35,6 +40,7 @@ app = do
         Spock.setStatus Http.status501
 
     Spock.get ("hello" <//> Spock.var) $ \name -> do
-        (DummyAppState ref) <- Spock.getState
-        visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-        Spock.text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+        -- (DummyAppState ref) <- Spock.getState
+        -- visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
+        -- Spock.text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+        Spock.text ("Hello " <> name)
