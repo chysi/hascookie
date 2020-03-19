b1bcf4eea4e3837485bd79f913d181f390af007e
2mol <2mol@users.noreply.github.com>
2020-03-19 13:23:12 +0100

    wip, index html

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,14 @@
 
 module Main where
 
--- import           Data.IORef (IORef, atomicModifyIORef', newIORef)
+
 import           Control.Monad.Trans (liftIO)
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import           Database.SQLite.Simple (Connection)
 import qualified DB
 import qualified Network.HTTP.Types as Http
-import           Types
+-- import           Types
 import           Web.Spock ((<//>))
 import qualified Web.Spock as Spock
 import qualified Web.Spock.Config as SC
@@ -25,39 +26,36 @@ type Server a = Spock.SpockM ServerDBConn ServerSession ServerState a
 
 main :: IO ()
 main = do
-    -- ref <- newIORef 0
     dbConnection <- DB.init "orders.db"
     let serverState = ServerState { dbConnection = dbConnection }
     spockCfg <- SC.defaultSpockCfg ServerSession SC.PCNoDatabase serverState
-    Spock.runSpock 8080 (Spock.spock spockCfg app)
+    frontPage <- TIO.readFile "site/index.html"
+    Spock.runSpock 8080 $ Spock.spock spockCfg (app frontPage)
 
 
-app :: Server ()
-app = do
+app :: T.Text -> Server ()
+app frontPage = do
     Spock.get Spock.root $ do
-        liftIO $ putStrLn ">> getting main page"
-        Spock.html "<h1 style=\"color:red\">Hello World!</h1>"
+        liftIO $ putStrLn ">> main page requested"
+        Spock.html frontPage
 
     Spock.get "heartbeat" $ do
         Spock.setStatus Http.status200
 
     Spock.post "icanhascookie" $ do
-        liftIO $ putStrLn ">> posting order"
-        params <- Spock.paramsPost
-        let maybeOrderData = parseOrderBody params
+        liftIO $ putStrLn ">> order posted"
+        -- params <- Spock.paramsPost
+        -- let maybeOrderData = parseOrderBody params
         if False
             then do
                 (ServerState dbConnection) <- Spock.getState
                 liftIO $ DB.addOrder dbConnection 12345
-            else pure ()
-        Spock.text $ maybe "Invalid form" (T.pack . show) maybeOrderData
+                Spock.setStatus Http.status200
+            else
+                Spock.setStatus Http.status500
+        -- Spock.text $ maybe "Invalid form" (T.pack . show) maybeOrderData
 
-    Spock.get ("orderstatus" <//> Spock.var) $ \orderNum -> do
-        liftIO $ putStrLn $ ">> getting status for order nr. " <> show (orderNum :: Int)
+    Spock.get ("orderstatus" <//> Spock.var) $ \orderId -> do
+        liftIO $ putStrLn $ ">> status for order nr. " <> show (orderId :: Int)
+        -- TODO
         Spock.setStatus Http.status501
-
-    -- Spock.get ("hello" <//> Spock.var) $ \name -> do
-        -- (DummyAppState ref) <- Spock.getState
-        -- visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-        -- Spock.text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
-        -- Spock.text ("Hello " <> name)
