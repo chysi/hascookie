61062e6775b024fdb903715fa4a5d676aee02a1f
2mol <2mol@users.noreply.github.com>
2020-03-21 12:42:05 +0100

    formatting

--- src/DB.hs
+++ src/DB.hs
@@ -2,28 +2,28 @@
 
 module DB where
 
-import qualified Types                         as T
-import qualified Data.Text                     as Text
-import qualified Database.SQLite.Simple        as Sql
+import qualified Data.Text as Text
+import qualified Database.SQLite.Simple as Sql
+import qualified Types as T
 -- import Database.SQLite.Simple (NamedParam(..))
 -- import Database.SQLite.Simple.FromRow (FromRow)
 
 
 init :: String -> IO Sql.Connection
 init dbFileName = do
-        conn          <- Sql.open dbFileName
-        (ver : _) : _ <-
-                Sql.query_ conn "select sqlite_version()" :: IO [[String]]
-        putStrLn $ "sqlite version " <> ver
-        Sql.execute_
-                conn
-                "create table if not exists orders (id integer primary key, amount integer, email text, address text, delivery_time text)"
-        pure conn
+    conn <- Sql.open dbFileName
+    (ver : _) : _ <- Sql.query_ conn "select sqlite_version()" :: IO [[String]]
+    putStrLn $ "sqlite version " <> ver
+    Sql.execute_ conn
+        "create table if not exists \
+        \ orders (id integer primary key, amount integer, \
+        \ email text, address text, delivery_time text)"
+    pure conn
 
 
 addOrder :: Sql.Connection -> T.OrderBody -> IO ()
-addOrder conn order = Sql.execute
-        conn
+addOrder conn order =
+    Sql.execute conn
         "insert into orders (amount, email, address, delivery_time) values (?, ?, ?, ?)"
         ( T.amount order :: Int
         , T.email order :: Text.Text
