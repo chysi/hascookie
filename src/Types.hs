c7984a1b22d89dfcc088cab64995380246185181
2mol <2mol@users.noreply.github.com>
2020-03-21 12:18:33 +0100

    formatting

--- src/Types.hs
+++ src/Types.hs
@@ -1,42 +1,43 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Types where
 
-import GHC.Generics
-
 import qualified Data.Aeson as A
+-- import qualified Data.List as List
 import qualified Data.Text as T
-import qualified Data.List as List
-import Text.Read (readMaybe)
+import GHC.Generics (Generic)
+-- import Text.Read (readMaybe)
 
-data OrderBody = OrderBody { email :: T.Text
-                 , address :: T.Text
-                 , amount :: Int
-                 , deliveryTime :: T.Text
-                 }
-  deriving (Show, Eq, Ord, Generic)
+data OrderBody = OrderBody
+    { email :: T.Text
+    , address :: T.Text
+    , amount :: Int
+    , deliveryTime :: T.Text
+    }
+    deriving (Show, Eq, Ord, Generic)
 
 customOptions = A.defaultOptions { A.fieldLabelModifier = A.camelTo2 '_' }
 
 instance A.FromJSON OrderBody where
-  parseJSON = A.genericParseJSON customOptions
+    parseJSON = A.genericParseJSON customOptions
 instance A.ToJSON OrderBody where
-  toEncoding = A.genericToEncoding customOptions
+    toEncoding = A.genericToEncoding customOptions
 
-data MessageBlock =
-  TextBlock T.Text
-  | ActionsBlock [MessageBlock]
-  | ButtonBlock T.Text
+-- data MessageBlock
+--     = TextBlock T.Text
+--     | ActionsBlock [MessageBlock]
+--     | ButtonBlock T.Text
 
 
-parseOrderBody :: [(T.Text, T.Text)] -> Maybe OrderBody
-parseOrderBody params =
-  case List.sortOn fst params of
-    [("amount", amount),
-     ("deliveryTime", deliveryTime),
-     ("email", email), ("address", address)] ->
-      do
-        parsedAmount <- readMaybe $ T.unpack amount
-        return $ OrderBody email address parsedAmount deliveryTime
-    _ ->
-      Nothing
+-- parseOrderBody :: [(T.Text, T.Text)] -> Maybe OrderBody
+-- parseOrderBody params =
+--   case List.sortOn fst params of
+--     [("amount", amount),
+--      ("deliveryTime", deliveryTime),
+--      ("email", email), ("address", address)] ->
+--       do
+--         parsedAmount <- readMaybe $ T.unpack amount
+--         return $ OrderBody email address parsedAmount deliveryTime
+--     _ ->
+--       Nothing
