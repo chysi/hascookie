74a7c76ec6ef87f9b91aa8d7709a9a698700f03d
2mol <2mol@users.noreply.github.com>
2020-03-21 11:19:57 +0100

    formatting

--- src/Main.hs
+++ src/Main.hs
@@ -4,42 +4,40 @@
 module Main where
 
 
-import           Control.Monad.Trans            ( liftIO )
-import qualified Network.HTTP.Types            as Http
-import qualified Network.Wai.Middleware.RequestLogger
-                                               as RequestLogger
+import Control.Monad.Trans (liftIO)
+import qualified Network.HTTP.Types as Http
+import qualified Network.Wai.Middleware.RequestLogger as RequestLogger
 import qualified Network.Wai.Middleware.Static as Static
-import qualified Web.Scotty                    as Scotty
+import qualified Web.Scotty as Scotty
 
 import qualified DB
-import qualified Types                         as T
+import qualified Types as T
 
 
 main :: IO ()
 main = do
-        dbConnection <- DB.init "orders.db"
+    dbConnection <- DB.init "orders.db"
 
-        Scotty.scotty 8080 $ do
-                Scotty.middleware RequestLogger.logStdoutDev
-                Scotty.middleware $ Static.staticPolicy (Static.addBase "site")
-                Scotty.get "/" $
-                    -- liftIO $ putStrLn ">> main page requested"
-                        Scotty.file "site/index.html"
+    Scotty.scotty 8080 $ do
+        Scotty.middleware RequestLogger.logStdoutDev
+        Scotty.middleware $ Static.staticPolicy (Static.addBase "site")
 
-                Scotty.get "/hello-app" $
-                        Scotty.text "I'm alive, thanks for asking!"
+        Scotty.get "/" $
+            Scotty.file "site/index.html"
 
+        Scotty.get "/hello-app" $
+            Scotty.text "I'm alive, thanks for asking!"
 
-                Scotty.post "icanhascookie" $ do
-                        liftIO $ putStrLn ">> order posted"
-                        (params :: T.OrderBody) <- Scotty.jsonData
-                        liftIO $ DB.addOrder dbConnection params
-                        Scotty.status Http.status200
-                        Scotty.text "Order submitted"
+        Scotty.post "/icanhascookie" $ do
+            liftIO $ putStrLn ">> order posted"
+            (params :: T.OrderBody) <- Scotty.jsonData
+            liftIO $ DB.addOrder dbConnection params
+            Scotty.status Http.status200
+            Scotty.text "Order submitted"
 
-                Scotty.get "/orderstatus/:order_id" $ do
-                        orderId <- Scotty.param "order_id" :: Scotty.ActionM Int
-                        liftIO $ putStrLn $ ">> status for order nr. " <> show
-                                (orderId :: Int)
-                        -- TODO: implement db call
-                        Scotty.status Http.status501
+        Scotty.get "/orderstatus/:order_id" $ do
+            orderId <- Scotty.param "order_id" :: Scotty.ActionM Int
+            liftIO $ putStrLn $ ">> status for order nr. " <> show
+                    (orderId :: Int)
+            -- TODO: implement db call
+            Scotty.status Http.status501
