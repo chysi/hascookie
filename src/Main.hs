99da6e76cf332234a7fbd9c651eba366bd88fa90
2mol <2mol@users.noreply.github.com>
2020-03-23 11:07:35 +0100

    formatting

--- src/Main.hs
+++ src/Main.hs
@@ -4,58 +4,65 @@
 module Main where
 
 
-import           Control.Monad.Trans            ( liftIO )
-import qualified Data.Text                     as Tx
-import qualified Data.Text.IO                  as Tx
-import qualified Network.HTTP.Types            as Http
-import qualified Network.Wai.Middleware.RequestLogger
-                                               as RequestLogger
+import Control.Monad.Trans (liftIO)
+import qualified Data.Text as Tx
+import qualified Data.Text.IO as Tx
+import qualified Network.HTTP.Types as Http
+import qualified Network.Wai.Middleware.RequestLogger as RequestLogger
 import qualified Network.Wai.Middleware.Static as Static
-import qualified System.Environment            as Env
-import qualified Web.Scotty                    as Scotty
+import qualified System.Environment as Env
+import qualified Web.Scotty as Scotty
 
 import qualified DB
 import qualified Slack
-import qualified Types                         as T
+import qualified Types as T
 
 
 envParseConfig :: IO (Either Tx.Text T.Config)
 envParseConfig = do
-  maybe_channel_id  <- Env.lookupEnv "HASCOOKIE_SLACK_CHANNEL_ID"
-  maybe_oauth_token <- Env.lookupEnv "HASCOOKIE_SLACK_TOKEN"
-  return $ T.parseConfig maybe_channel_id maybe_oauth_token
+    maybeChannelId  <- Env.lookupEnv "HASCOOKIE_SLACK_CHANNEL_ID"
+    maybeOauthToken <- Env.lookupEnv "HASCOOKIE_SLACK_TOKEN"
+    pure $ T.parseConfig maybeChannelId maybeOauthToken
+
 
 main :: IO ()
 main = do
-  dbConnection <- DB.init "orders.db"
-  maybeConfig  <- envParseConfig
-  case maybeConfig of
-    Left  err    -> Tx.putStrLn err
-    Right config -> Scotty.scotty 8080 $ do
-      Scotty.middleware RequestLogger.logStdoutDev
-      Scotty.middleware $ Static.staticPolicy (Static.addBase "site")
+    dbConnection <- DB.init "orders.db"
 
-      Scotty.get "/" $ Scotty.file "site/index.html"
+    -- maybeChannelId <- Env.lookupEnv "HASCOOKIE_SLACK_CHANNEL_ID"
+    -- maybeAuthToken <- Env.lookupEnv "HASCOOKIE_SLACK_TOKEN"
 
-      Scotty.get "/hello-app" $ Scotty.text "I'm alive, thanks for asking!"
+    maybeConfig  <- envParseConfig
 
-      Scotty.post "/icanhascookie" $ do
-        liftIO $ putStrLn ">> order posted"
-        (params :: T.OrderBody) <- Scotty.jsonData
-        liftIO $ DB.addOrder dbConnection params
-        slack_request_status <-
-          liftIO $ Slack.makeSlackRequest config $ Slack.generateMessage
-            config
-            params
-        liftIO
-          $  Tx.putStrLn
-          $  ">> Slack request response: "
-          <> slack_request_status
-        Scotty.status Http.status200
-        Scotty.text "Order submitted"
+    case maybeConfig of
+        Left  err    -> Tx.putStrLn err
+        Right config -> Scotty.scotty 8080 $ do
+            Scotty.middleware RequestLogger.logStdoutDev
+            Scotty.middleware $ Static.staticPolicy (Static.addBase "site")
 
-      Scotty.get "/orderstatus/:order_id" $ do
-        orderId <- Scotty.param "order_id" :: Scotty.ActionM Int
-        liftIO $ putStrLn $ ">> status for order nr. " <> show (orderId :: Int)
-        -- TODO: implement db call
-        Scotty.status Http.status501
+            Scotty.get "/" $
+                Scotty.file "site/index.html"
+
+            Scotty.get "/hello-app" $
+                Scotty.text "I'm alive, thanks for asking!"
+
+            Scotty.post "/icanhascookie" $ do
+                liftIO $ putStrLn ">> order posted"
+                (params :: T.OrderBody) <- Scotty.jsonData
+                liftIO $ DB.addOrder dbConnection params
+                slack_request_status <-
+                    liftIO $ Slack.makeSlackRequest config $ Slack.generateMessage
+                        config
+                        params
+                liftIO
+                    $  Tx.putStrLn
+                    $  ">> Slack request response: "
+                    <> slack_request_status
+                Scotty.status Http.status200
+                Scotty.text "Order submitted"
+
+            Scotty.get "/orderstatus/:order_id" $ do
+                orderId <- Scotty.param "order_id" :: Scotty.ActionM Int
+                liftIO $ putStrLn $ ">> status for order nr. " <> show (orderId :: Int)
+                -- TODO: implement db call
+                Scotty.status Http.status501
--- src/Types.hs
+++ src/Types.hs
@@ -3,48 +3,49 @@
 
 module Types where
 
-import qualified Data.Aeson                    as A
-import qualified Data.Text                     as Tx
-import qualified Data.List                     as List
-import           Text.Read                      ( readMaybe )
-import           GHC.Generics                   ( Generic )
+import qualified Data.Aeson as A
+-- import qualified Data.List as List
+import qualified Data.Text as Tx
+import GHC.Generics (Generic)
+-- import Text.Read (readMaybe)
 
 data Config = Config
-  { slackChannelId :: Tx.Text
-  , slackOauthToken :: Tx.Text
-  }
+    { slackChannelId :: Tx.Text
+    , slackOauthToken :: Tx.Text
+    }
 
 data OrderBody = OrderBody
-  { email :: Tx.Text
-  , address :: Tx.Text
-  , amount :: Int
-  , deliveryTime :: Tx.Text
-  }
-  deriving (Show, Eq, Ord, Generic)
+    { email :: Tx.Text
+    , address :: Tx.Text
+    , amount :: Int
+    , deliveryTime :: Tx.Text
+    }
+    deriving (Show, Eq, Ord, Generic)
 
-customOptions = A.defaultOptions { A.fieldLabelModifier = A.camelTo2 '_' }
+customOptions =
+    A.defaultOptions { A.fieldLabelModifier = A.camelTo2 '_' }
 
 instance A.FromJSON OrderBody where
-  parseJSON = A.genericParseJSON customOptions
+    parseJSON = A.genericParseJSON customOptions
 instance A.ToJSON OrderBody where
-  toEncoding = A.genericToEncoding customOptions
+    toEncoding = A.genericToEncoding customOptions
 
 data SlackMessagePayload = SlackMessagePayload
-  { channel :: Tx.Text
-  , text :: Tx.Text
-  }
-  deriving (Show, Eq, Ord, Generic)
+    { channel :: Tx.Text
+    , text :: Tx.Text
+    }
+    deriving (Show, Eq, Ord, Generic)
 
 instance A.ToJSON SlackMessagePayload where
-  toEncoding = A.genericToEncoding customOptions
+    toEncoding = A.genericToEncoding customOptions
 
 parseConfig :: Maybe String -> Maybe String -> Either Tx.Text Config
 parseConfig maybe_channel_id maybe_oauth_token = case maybe_channel_id of
-  Nothing -> Left "Missing environment variable: HASCOOKIE_SLACK_CHANNEL_ID"
-  Just channel_id -> case maybe_oauth_token of
-    Nothing -> Left "Missing environment variable: HASCOOKIE_SLACK_TOKEN"
-    Just oauth_token ->
-      Right $ Config (Tx.pack channel_id) (Tx.pack oauth_token)
+    Nothing -> Left "Missing environment variable: HASCOOKIE_SLACK_CHANNEL_ID"
+    Just channel_id -> case maybe_oauth_token of
+        Nothing -> Left "Missing environment variable: HASCOOKIE_SLACK_TOKEN"
+        Just oauth_token ->
+            Right $ Config (Tx.pack channel_id) (Tx.pack oauth_token)
 
 -- data MessageBlock =
 --   TextBlock Tx.Text
