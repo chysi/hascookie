079cef908ba82a85f78f7af0622eba34b6333968
2mol <2mol@users.noreply.github.com>
2020-03-23 11:11:04 +0100

    pushing back config check into ordering endpoint

--- src/Main.hs
+++ src/Main.hs
@@ -34,35 +34,42 @@ main = do
 
     maybeConfig  <- envParseConfig
 
-    case maybeConfig of
-        Left  err    -> Tx.putStrLn err
-        Right config -> Scotty.scotty 8080 $ do
-            Scotty.middleware RequestLogger.logStdoutDev
-            Scotty.middleware $ Static.staticPolicy (Static.addBase "site")
+    -- case maybeConfig of
+    --     Left  err    -> Tx.putStrLn err
+    --     Right config ->
+    Scotty.scotty 8080 $ do
+        Scotty.middleware RequestLogger.logStdoutDev
+        Scotty.middleware $ Static.staticPolicy (Static.addBase "site")
 
-            Scotty.get "/" $
-                Scotty.file "site/index.html"
+        Scotty.get "/" $
+            Scotty.file "site/index.html"
 
-            Scotty.get "/hello-app" $
-                Scotty.text "I'm alive, thanks for asking!"
+        Scotty.get "/hello-app" $
+            Scotty.text "I'm alive, thanks for asking!"
 
-            Scotty.post "/icanhascookie" $ do
-                liftIO $ putStrLn ">> order posted"
-                (params :: T.OrderBody) <- Scotty.jsonData
-                liftIO $ DB.addOrder dbConnection params
-                slack_request_status <-
-                    liftIO $ Slack.makeSlackRequest config $ Slack.generateMessage
-                        config
-                        params
-                liftIO
-                    $  Tx.putStrLn
-                    $  ">> Slack request response: "
-                    <> slack_request_status
-                Scotty.status Http.status200
-                Scotty.text "Order submitted"
+        Scotty.post "/icanhascookie" $ do
+            liftIO $ putStrLn ">> order posted"
+            (params :: T.OrderBody) <- Scotty.jsonData
+            liftIO $ DB.addOrder dbConnection params
 
-            Scotty.get "/orderstatus/:order_id" $ do
-                orderId <- Scotty.param "order_id" :: Scotty.ActionM Int
-                liftIO $ putStrLn $ ">> status for order nr. " <> show (orderId :: Int)
-                -- TODO: implement db call
-                Scotty.status Http.status501
+            case maybeConfig of
+                Left err -> do
+                    liftIO $ putStrLn ">> Skipping Slack notification."
+                    liftIO $ Tx.putStrLn err
+                Right config -> do
+                    slack_request_status <-
+                        liftIO $ Slack.makeSlackRequest config $ Slack.generateMessage
+                            config
+                            params
+                    liftIO
+                        $  Tx.putStrLn
+                        $  ">> Slack request response: "
+                        <> slack_request_status
+            Scotty.status Http.status200
+            Scotty.text "Order submitted"
+
+        Scotty.get "/orderstatus/:order_id" $ do
+            orderId <- Scotty.param "order_id" :: Scotty.ActionM Int
+            liftIO $ putStrLn $ ">> status for order nr. " <> show (orderId :: Int)
+            -- TODO: implement db call
+            Scotty.status Http.status501
